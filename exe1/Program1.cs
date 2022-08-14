ea784aaf3321f01b7a6b2d40f5f4b1d6c5313ab9
Oleg <you@example.com>
2022-08-14 08:33:22 +0700

    first commit

---
+++ exe1/Program1.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Число a = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Число b = ");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("max = " + a + ", min = " + b);    
+} else {Console.WriteLine("max = " + b + ", min = " + a);}
+
+Console.Write("Число a = ");
+int a1 = int.Parse(Console.ReadLine());
+Console.Write("Число b = ");
+int b1 = int.Parse(Console.ReadLine());
+Console.Write("Число c = ");
+int c1 = int.Parse(Console.ReadLine());
+int max = a1;
+if (max < b1) {
+    max = b1;     
+} 
+if (max < c1) {
+    max = c1;
+}
+Console.WriteLine("max = " + max);
+
+Console.Write("Число для проверки = ");
+int a2 = int.Parse(Console.ReadLine());
+if (a2 % 2 == 0)
+{
+    Console.WriteLine(a2 + "- четное");
+} else {Console.WriteLine(a2 + "- НЕ четное");}
+
+Console.Write("Число N = ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i < n+1; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + ", ");
+    } 
+}
---
+++ exe2/Program.cs
@@ -0,0 +1,28 @@
+﻿int GetNum(string message)
+{
+    Console.Write(message);
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
+
+int userNum = GetNum("Введите трехзначное число: ");
+Console.WriteLine(userNum / 10 % 10);
+
+int random = new Random().Next(100, 999);
+Console.WriteLine("Случайное трехзначное число: " + random);
+Console.WriteLine("     Число без второй цифры: " + random / 100 % 10 + " " + random % 10);
+
+int userNum1 = GetNum("Введите число: ");
+if (userNum1 / 100 > 0)
+{
+    Console.WriteLine("третья цифра: " + userNum1 % 10);
+} else
+{
+    Console.WriteLine("третьей цифры нет!");
+}
+
+int userNum2 = GetNum("Введите день недели числом: ");
+if (userNum2 == 6 ^ userNum2 == 7)
+{
+    Console.WriteLine("Выходной!");
+}
