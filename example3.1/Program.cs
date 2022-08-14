65fa35075d6093bd1633e2138e87049e56a23724
Oleg <you@example.com>
2022-08-14 12:34:33 +0700

    example 3.1 ready

--- example3.1/Program.cs
+++ example3.1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNum(string message)
+{
+    Console.Write(message);
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
+
+int GetDigit(int value, int digit)
+{
+    int[] stack = new int[5];
+    int index = 0;
+    while(value > 0) {
+        stack[index] = value % 10; 
+        value = value/10;
+        index++;     
+    }  
+    return stack[digit-1];
+}
+
+int userNum = GetNum("Введите пятизначное число: ");
+
+if (userNum > 9999 && userNum < 1000000)
+{
+    if (GetDigit(userNum,1) == GetDigit(userNum,5) && GetDigit(userNum,2) == GetDigit(userNum,4))
+    {
+        Console.WriteLine("Число палиндром!");            
+    } else
+    {
+        Console.WriteLine("Число НЕ палиндром!");
+    }
+} else
+{
+    Console.WriteLine("Число не пятизначное"); 
+}
