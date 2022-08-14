9840cbf5eb7f8de5f90210716198e0392f329bab
Oleg <you@example.com>
2022-08-14 13:30:43 +0700

    example 3.3 ready

--- example3.3/Program.cs
+++ example3.3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int GetNum(string message)
+{
+    Console.Write(message);
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
+
+double cube(int n)
+{
+    return Math.Pow(n, 3);
+}
+
+int n = GetNum("Введите число N:");
+
+Console.Write("Кубы чисел 1-N:");
+for (int i = 1; i <= n; i++)
+{
+    Console.Write(cube(i) + ", ");
+}
