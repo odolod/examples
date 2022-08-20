eb75566dee597c176f544420f98b70e80ce6b061
Oleg <you@example.com>
2022-08-20 09:45:28 +0700

    example 5.2 ready

--- example5.2/Program.cs
+++ example5.2/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNum(string message)
+{
+    Console.Write(message);
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
+
+string PrintArray(int[] array)
+{
+    string res = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        res = res + array[i];
+        if (i < array.Length-1)
+        {
+            res = res + ", ";
+        }
+    }
+    res = res + "]";
+    return res;
+}
+
+void FillArrayRnd(int[] array, Random rnd, int minValue, int maxValue)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minValue, maxValue);
+    }
+}
+
+Random rnd = new Random();
+int size = rnd.Next(3,15);
+int[] array = new int[size];
+FillArrayRnd(array, rnd, -100, 100);
+
+int answer = 0;
+
+for (int i = 1; i < size; i = i+2)
+{
+    answer = answer + array[i];
+}
+
+Console.Write("Масив:"+PrintArray(array));
+Console.Write(", сумма чисел на нечетных позициях: " + answer);
+
