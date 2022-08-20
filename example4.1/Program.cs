78ca8fa3ab04dd64eb39f97af89c35c4d66e37a6
Oleg <you@example.com>
2022-08-20 08:04:32 +0700

    example 4.1 ready

--- example4.1/Program.cs
+++ example4.1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int GetNum(string message)
+{
+    Console.Write(message);
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
+
+int a = GetNum("Введите число которое будет возведено в степень:");
+int b = GetNum("Введите степень:");
+
+if (b < 0)
+{
+    Console.Write(b + " не 0 и не натуральное число!");
+} else 
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.Write(a + " в степени " + b + " равно: " + result);
+}
