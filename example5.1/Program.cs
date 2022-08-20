faf2700cf42aca6884d58b7ef4f6b68b7f6d0f5f
Oleg <you@example.com>
2022-08-20 09:40:15 +0700

    example 5.1 ready

--- example5.1/Program.cs
+++ example5.1/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNum(string message)
+{
+    Console.Write(message);
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
+
+string PrintArray(int[] array)
+{
+    string res = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        res = res + array[i];
+        if (i < array.Length-1)
+        {
+            res = res + ", ";
+        }
+    }
+    res = res + "]";
+    return res;
+}
+
+void FillArrayRnd(int[] array, Random rnd, int minValue, int maxValue)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minValue, maxValue);
+    }
+}
+
+Random rnd = new Random();
+int size = rnd.Next(3,15);
+int[] array = new int[size];
+FillArrayRnd(array, rnd, 100, 999);
+
+int answer = 0;
+
+for (int i = 0; i < size; i++)
+{
+    if (array[i] % 2 == 0) answer++;
+}
+
+Console.Write("Масив:"+PrintArray(array));
+Console.Write(", четных чисел: " + answer);
