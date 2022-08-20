b01df71a23c22b1f44934ca9d90bedd72786e6cd
Oleg <you@example.com>
2022-08-20 08:11:24 +0700

    example 4.2 ready

--- example4.2/Program.cs
+++ example4.2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNum(string message)
+{
+    Console.Write(message);
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
+
+int n = GetNum("Введите число N:");
+
+Console.Write("Сумма цифр N:");
+
+int sum = 0;
+int index = 0;
+while(n > 0) {
+    sum = sum + n % 10; 
+    n = n/10;
+    index++;     
+}  
+
+Console.Write(sum);
