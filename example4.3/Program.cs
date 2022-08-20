6ad0cd884896a59e3bec97e9a95cf381385fd49e
Oleg <you@example.com>
2022-08-20 08:32:20 +0700

    example 4.3 ready

--- example4.3/Program.cs
+++ example4.3/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNum(string message)
+{
+    Console.Write(message);
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
+
+string PrintArray(int[] array)
+{
+    string res = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        res = res + array[i];
+        if (i < array.Length-1)
+        {
+            res = res + ", ";
+        }
+    }
+    res = res + "]";
+    return res;
+}
+
+int[] array = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+    int index = i+1;
+    array[i] = GetNum("Введите " + index + "-e число массива:");
+}
+
+Console.Write("Масив:"+PrintArray(array));
+Array.Sort(array);
+Console.Write(", второй максимум: " + array[6]);
+
+
+
+/* Console.Write("Сумма цифр N:");
+
+int sum = 0;
+int index = 0;
+while(n > 0) {
+    sum = sum + n % 10; 
+    n = n/10;
+    index++;     
+}  
+
+Console.Write(sum); */
