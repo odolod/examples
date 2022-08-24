717f759afde64ae8ebf99ed74d5578db5408785a
Oleg <you@example.com>
2022-08-24 07:54:05 +0700

    example 6.1 ready

--- example6.1/Program.cs
+++ example6.1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double GetNum(string message)
+{
+    Console.Write(message);
+    double res = double.Parse(Console.ReadLine());
+    return res;
+}
+
+int NotZeroCount(double[] array)
+{
+    int res = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) res++;
+    }
+    return res;
+}
+
+int m = 5;
+double[] array = new double[m];
+
+Console.WriteLine("Введите M="+m+" чисел:");
+for (int i = 0; i < m; i++)
+{
+    array[i] = GetNum("Введите число:");
+}
+
+Console.Write("Введено чисел больше нуля:"+NotZeroCount(array));
+
+
