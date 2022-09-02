fec3387e1fae7ebf523f5f1148f1fc4e9571bbea
Oleg <you@example.com>
2022-09-02 10:40:04 +0700

    example 9.3 ready

--- example9.3/Program.cs
+++ example9.3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNum(string message)
+{
+    Console.Write(message);
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
+
+int A(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1, A(n, m - 1));
+}
+
+int n = GetNum("Задайте число N:");
+int m = GetNum("Задайте число M:");
+Console.Write("A(N,M)="+A(n,m).ToString());
